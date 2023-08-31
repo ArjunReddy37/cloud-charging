58bb7aab6b44781d6b5318eaa03ea3d2a555e578
ArjunReddy37 <75614698+ArjunReddy37@users.noreply.github.com>
2023-08-31 19:46:42 +0530

    Improving business logic in app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -4,13 +4,17 @@ import { json } from "body-parser";
 
 const DEFAULT_BALANCE = 100;
 
+let reusableRedisClient: ReturnType<typeof createClient> | null = null;
+const reuseRedisClient = true;
+const pingRedisClient = false;
+
 interface ChargeResult {
     isAuthorized: boolean;
     remainingBalance: number;
     charges: number;
 }
 
-async function connect(): Promise<ReturnType<typeof createClient>> {
+async function createReusableRedisClient(): Promise<ReturnType<typeof createClient>> {
     const url = `redis://${process.env.REDIS_HOST ?? "localhost"}:${process.env.REDIS_PORT ?? "6379"}`;
     console.log(`Using redis URL ${url}`);
     const client = createClient({ url });
@@ -18,28 +22,60 @@ async function connect(): Promise<ReturnType<typeof createClient>> {
     return client;
 }
 
+async function connect(): Promise<ReturnType<typeof createClient>> {
+    if (reusableRedisClient == null) {
+        reusableRedisClient = await createReusableRedisClient();
+        return reusableRedisClient;
+    }
+    try {
+        if (pingRedisClient) {
+            await reusableRedisClient.ping();
+        }
+    } catch (error) {
+        console.warn("failed to re-use existing redis client");
+        reusableRedisClient = await createReusableRedisClient();
+    }
+    return reusableRedisClient;
+}
+
+
+async function disconnect(client: ReturnType<typeof createClient>): Promise<void> {
+    if (!reuseRedisClient) {
+        /*await */client.disconnect();
+    }
+}
+
 async function reset(account: string): Promise<void> {
     const client = await connect();
     try {
         await client.set(`${account}/balance`, DEFAULT_BALANCE);
     } finally {
-        await client.disconnect();
+        await disconnect(client);
     }
 }
 
 async function charge(account: string, charges: number): Promise<ChargeResult> {
     const client = await connect();
     try {
-        const balance = parseInt((await client.get(`${account}/balance`)) ?? "");
-        if (balance >= charges) {
-            await client.set(`${account}/balance`, balance - charges);
-            const remainingBalance = parseInt((await client.get(`${account}/balance`)) ?? "");
+        const remainingBalance = await client.decrBy(`${account}/balance`, charges);
+        if (remainingBalance >= 0) {
             return { isAuthorized: true, remainingBalance, charges };
-        } else {
-            return { isAuthorized: false, remainingBalance: balance, charges: 0 };
         }
+        // Rollback charges
+        const balance = await client.incrBy(`${account}/balance`, charges);
+        return { isAuthorized: false, remainingBalance: balance, charges: 0 };
     } finally {
-        await client.disconnect();
+        await disconnect(client);
+    }
+}
+
+async function balance(account: string): Promise<ChargeResult> {
+    const client = await connect();
+    try {
+        const remainingBalance = parseInt(await client.get(`${account}/balance`) ?? "");
+        return { isAuthorized: false, remainingBalance: remainingBalance, charges: 0 };
+    } finally {
+        await disconnect(client);
     }
 }
 
@@ -61,12 +97,29 @@ export function buildApp(): express.Application {
         try {
             const account = req.body.account ?? "account";
             const result = await charge(account, req.body.charges ?? 10);
-            console.log(`Successfully charged account ${account}`);
+            console.log(`Successfully charged account ${account}: remainingBalance: ${result.remainingBalance}, isAuthorized: ${result.isAuthorized}`);
             res.status(200).json(result);
         } catch (e) {
             console.error("Error while charging account", e);
             res.status(500).json({ error: String(e) });
         }
     });
+    app.get("/balance", async (req, res) => {
+        try {
+            const account = req.body.account ?? "account";
+            const result = await balance(account);
+            console.log(`Successfully fetched balance for account ${account}: remainingBalance: ${result.remainingBalance}`);
+            res.status(200).json(result);
+        } catch (e) {
+            console.error("Error while fetching balance for account", e);
+            res.status(500).json({ error: String(e) });
+        }
+    });
+    app.on('SIGTERM', () => {
+        console.log('SIGTERM signal received: closing REDIS client')
+        if (reusableRedisClient != null) {
+            reusableRedisClient.disconnect();
+        }
+      })
     return app;
 }
